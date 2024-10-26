7516cb4b9d1e7ab4a38dfc662b21f51e5756d2b3
Gabriela25 <gabrielabarreto25@gmail.com>
2024-10-25 20:28:00 -0500

    add url

--- src/service-worker.js
+++ src/service-worker.js
@@ -8,8 +8,29 @@ self.addEventListener('push', function(event) {
       body: data.body || 'Tienes una nueva notificación',
       icon: 'assets/logo.png'
     };
-  
+    event.notification.close()
+    const urlToOpen = data.url || 'http://localhost:1200/';
+    
     event.waitUntil(
       self.registration.showNotification(title, options)
+    );
+  });
+  self.addEventListener("notificationclick", (event) => {
+    event.notification.close()
+    const urlToOpen = event.notification.data?.url || 'http://localhost:5200/';
+    event.waitUntil(
+      clients.matchAll({ type: 'window', includeUncontrolled: true }).then(windowClients => {
+       
+        for (const client of windowClients) {
+          console.log(client.url)
+          console.log(urlToOpen)
+          if (client.url === urlToOpen && 'focus' in client) {
+            return client.focus();
+          }
+        } 
+        if (clients.openWindow) {
+          return clients.openWindow(urlToOpen);
+        }
+      })
     );
   });
